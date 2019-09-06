a93630cd917a05fcf1d3ffb8bef05a29911eb036
Anmol Sethi <hi@nhooyr.io>
2019-09-06 13:35:35 -0500

    Uncomment testlog example

--- examples_test.go
+++ examples_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"go.coder.com/slog"
 	"go.coder.com/slog/stderrlog"
+	"go.coder.com/slog/testlog"
+	"testing"
 )
 
 func Example_stderr() {
@@ -23,21 +25,21 @@ func Example_stderr() {
 	)
 }
 
-// func Example_test() {
-// 	// Nil here but would be provided by the testing framework.
-// 	var t *testing.T
-//
-// 	testlog.Info(t, "my message here",
-// 		"field_name", "something or the other",
-// 		"some_map", map[string]interface{}{
-// 			"nested_fields": "wowow",
-// 		},
-// 		"some slice", []interface{}{
-// 			1,
-// 			"foof",
-// 			"bar",
-// 			true,
-// 		},
-// 		log.Component, "test",
-// 	)
-// }
+func Example_test() {
+	// Nil here but would be provided by the testing framework.
+	var t *testing.T
+
+	testlog.Info(t, "my message here",
+		"field_name", "something or the other",
+		"some_map", map[string]interface{}{
+			"nested_fields": "wowow",
+		},
+		"some slice", []interface{}{
+			1,
+			"foof",
+			"bar",
+			true,
+		},
+		slog.Component("test"),
+	)
+}
