1acb21f423ef48330c3921ba934571218eb8c92f
Colin Adler <colin@coder.com>
2019-12-10 15:01:14 -0600

    Fix function comments in assert package

--- sloggers/slogtest/assert/assert.go
+++ sloggers/slogtest/assert/assert.go
@@ -1,5 +1,5 @@
 // Package assert is a helper package for test assertions.
-package assert
+package assert // import "cdr.dev/slog/sloggers/slogtest/assert"
 
 import (
 	"testing"
@@ -11,8 +11,8 @@ import (
 
 // Equal asserts exp == act.
 //
-// If they are not equal, it will fatal the test
-// with a diff of the differences.
+// If they are not equal, it will fatal the test with a diff of the
+// two objects.
 func Equal(t testing.TB, exp, act interface{}, name string) {
 	slog.Helper()
 	if diff := assert.CmpDiff(exp, act); diff != "" {
@@ -24,6 +24,8 @@ func Equal(t testing.TB, exp, act interface{}, name string) {
 }
 
 // Success asserts err == nil.
+//
+// If err isn't nil, it will fatal the test with the error.
 func Success(t testing.TB, err error, name string) {
 	slog.Helper()
 	if err != nil {
@@ -34,7 +36,9 @@ func Success(t testing.TB, err error, name string) {
 	}
 }
 
-// True act == true.
+// True asserts act == true.
+//
+// If act isn't true, it will fatal the test.
 func True(t testing.TB, act bool, name string) {
 	slog.Helper()
 	Equal(t, true, act, name)
