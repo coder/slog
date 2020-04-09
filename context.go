958f7eb3e915b963508e7f3bd23fa702ff673ffe
Anmol Sethi <hi@nhooyr.io>
2020-04-09 02:17:46 -0400

    Revert "Merge pull request #86 from cdr/sink-context-iface"
    This reverts commit e88ff39eeaafb6157a32889067ceb7caf0e7744f, reversing
    changes made to 95bc0f1f03c0bdd180fc467c42383cee7d46fcb4.

--- context.go
+++ context.go
@@ -4,21 +4,15 @@ import "context"
 
 type loggerCtxKey = struct{}
 
-type sinkContext struct {
-	context.Context
-	Sink
-}
-
-// SinkContext is a context that implements Sink.
-// It may be returned by log creators to allow for composition.
-type SinkContext interface {
+// SinkContext is used by slog.Make to compose many loggers together.
+type SinkContext struct {
 	Sink
 	context.Context
 }
 
 func contextWithLogger(ctx context.Context, l logger) SinkContext {
 	ctx = context.WithValue(ctx, loggerCtxKey{}, l)
-	return &sinkContext{
+	return SinkContext{
 		Context: ctx,
 		Sink:    l,
 	}
