5519752b31c469cf4e04c220ec381eea4341f953
Anmol Sethi <hi@nhooyr.io>
2020-08-28 12:29:57 -0400

    Fix slogstackdriver test
    Needs project in path now.

--- sloggers/slogstackdriver/slogstackdriver_test.go
+++ sloggers/slogstackdriver/slogstackdriver_test.go
@@ -28,9 +28,11 @@ func TestStackdriver(t *testing.T) {
 	l = l.Named("meow")
 	l.Error(ctx, "line1\n\nline2", slog.F("wowow", "me\nyou"))
 
+	projectID, _ := metadata.ProjectID()
+
 	j := entryjson.Filter(b.String(), "timestamp")
-	exp := fmt.Sprintf(`{"severity":"ERROR","message":"line1\n\nline2","logging.googleapis.com/sourceLocation":{"file":"%v","line":29,"function":"cdr.dev/slog/sloggers/slogstackdriver_test.TestStackdriver"},"logging.googleapis.com/operation":{"producer":"meow"},"logging.googleapis.com/trace":"projects//traces/%v","logging.googleapis.com/spanId":"%v","logging.googleapis.com/trace_sampled":false,"wowow":"me\nyou"}
-`, slogstackdriverTestFile, s.SpanContext().TraceID, s.SpanContext().SpanID)
+	exp := fmt.Sprintf(`{"severity":"ERROR","message":"line1\n\nline2","logging.googleapis.com/sourceLocation":{"file":"%v","line":30,"function":"cdr.dev/slog/sloggers/slogstackdriver_test.TestStackdriver"},"logging.googleapis.com/operation":{"producer":"meow"},"logging.googleapis.com/trace":"projects/%v/traces/%v","logging.googleapis.com/spanId":"%v","logging.googleapis.com/trace_sampled":false,"wowow":"me\nyou"}
+`, slogstackdriverTestFile, projectID, s.SpanContext().TraceID, s.SpanContext().SpanID)
 	assert.Equal(t, "entry", exp, j)
 }
 
