f808c13f1ed0a3fa03357e8fb7349fcd35a1d645
Kyle Carberry <kyle@coder.com>
2021-11-09 00:18:06 +0000

    Fix level

--- s.go
+++ s.go
@@ -8,15 +8,15 @@ import (
 
 // Stdlib creates a standard library logger from the given logger.
 //
-// All logs will be logged at the Info level and the given ctx
-// will be passed to the logger's Info method, thereby logging
-// all fields and tracing info in the context.
+// All logs will be logged at the level set by the logger and the
+// given ctx will be passed to the logger's Log method, thereby
+// logging all fields and tracing info in the context.
 //
 // You can redirect the stdlib default logger with log.SetOutput
 // to the Writer on the logger returned by this function.
 // See the example.
 func Stdlib(ctx context.Context, l Logger) *log.Logger {
-	l.skip += 3
+	l.skip += 2
 
 	l = l.Named("stdlib")
 
