95f32ea33e684cf2d51f4789c6b544e8424750a2
Anmol Sethi <hi@nhooyr.io>
2022-12-01 02:55:54 -0800

    test.mk: Fix for BSD sed

--- ci/test.mk
+++ ci/test.mk
@@ -19,4 +19,4 @@ coveralls: gotest
 
 gotest:
 	go test -covermode=count -coverprofile=ci/out/coverage.prof -coverpkg=./... $${GOTESTFLAGS-} ./...
-	sed -i '/internal\/assert/d' ci/out/coverage.prof
+	sed -i.bak '/internal\/assert/d' ci/out/coverage.prof
