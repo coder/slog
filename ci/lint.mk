8e82d917435795f1ab66ffb91ca9564c6206c13c
Mathias Fredriksson <mafredri@gmail.com>
2024-11-07 15:38:43 +0200

    Update lint.mk

--- ci/lint.mk
+++ ci/lint.mk
@@ -4,5 +4,5 @@ govet:
 	go vet ./...
 
 golint:
-	# golangci-lint v1.60.0 is not compatible with Go 1.20.
-	go run github.com/golangci/golangci-lint/cmd/golangci-lint@v1.59.1 run .
+	# golangci-lint newer than v1.55.2 is not compatible with Go 1.20 when using go run.
+	go run github.com/golangci/golangci-lint/cmd/golangci-lint@v1.55.2 run .
