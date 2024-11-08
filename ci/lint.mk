3e5cea5e4d6a8c8396f626a8a4989cc02aea4ed1
Mathias Fredriksson <mafredri@gmail.com>
2024-11-08 08:14:28 +0200

    ci: fix lint, fmt and upload artifacts (#218)
    * chore(.github): update actions/upload-artifacts

--- ci/lint.mk
+++ ci/lint.mk
@@ -4,4 +4,5 @@ govet:
 	go vet ./...
 
 golint:
-	go run github.com/golangci/golangci-lint/cmd/golangci-lint@latest run .
+	# golangci-lint newer than v1.55.2 is not compatible with Go 1.20 when using go run.
+	go run github.com/golangci/golangci-lint/cmd/golangci-lint@v1.55.2 run .
